6d47a012e29b13a27ed4c1ff7af36983ed440766
Jeyavelrajan Selvaraj <jeyavelrajan.s@itp.objectfrontier.com>
2024-01-17 18:12:01 +0530

    first commit

---
+++ src/main/java/com/rtz/SbSportsApplication.java
@@ -0,0 +1,13 @@
+package com.rtz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rtz/SbSportsApplicationTests.java
@@ -0,0 +1,25 @@
+package com.rtz;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.rts.evaluation.model.Sports;
+import com.rts.evaluation.reopsitory.SportsDAO;
+
+@SpringBootTest
+class SbSportsApplicationTests {
+
+
+	 static SportsDAO dao;
+	Sports bean = new Sports(134,"jeyavel","@123",23,234567891,"sjrajan11955@gmail.com");
+
+	@Test
+	public void testperformInsert() {
+		dao=new SportsDAO();
+		boolean result = dao.insert(bean);
+		assertEquals(result, true);
+	}
+}
